a60ba60a67b1f3a6abf155b9cc8b861e3d5382c0
Ana Sofía <anasofiasama@gmail.com>
2022-06-17 23:56:30 +0000

    Twitter PY

--- src/app.py
+++ src/app.py
@@ -6,3 +6,17 @@ load_dotenv()
 
 
 # your app code here
+consumer_key = os.environ.get('CONSUMER_KEY')
+
+import tweepy
+import requests
+
+# Creamos cliente
+
+client = tweepy.Client( bearer_token=bearer_token,
+consumer_key=consumer_key,
+consumer_secret=consumer_secret,
+return_type = requests.Response,
+wait_on_rate_limit=True)
+
+query = '#100daysofcode (pandas OR python) -is:retweet'
